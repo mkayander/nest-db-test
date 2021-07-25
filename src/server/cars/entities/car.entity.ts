508f5099e93b217ebf047415c7a2446a88bd1f83
Max Kayander <maxim.kayander1@gmail.com>
2021-07-25 21:38:34 +0300

    Implemented car entity.

--- src/server/cars/entities/car.entity.ts
+++ src/server/cars/entities/car.entity.ts
@@ -1,1 +1,26 @@
-export class Car {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum CarEngineType {
+  gas,
+  diesel,
+  electric,
+  hybrid,
+}
+
+@Entity()
+export class Car {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  manufacturer: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  engineType: CarEngineType;
+}
