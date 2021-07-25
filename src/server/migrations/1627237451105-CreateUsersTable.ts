175aeef45d99a2674faf632190f9315fac756448
Max Kayander <maxim.kayander1@gmail.com>
2021-07-25 21:38:47 +0300

    Formatting.

--- src/server/migrations/1627237451105-CreateUsersTable.ts
+++ src/server/migrations/1627237451105-CreateUsersTable.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateUsersTable1627237451105 implements MigrationInterface {
-    name = 'CreateUsersTable1627237451105'
+  name = "CreateUsersTable1627237451105";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE TABLE `user` (`id` int NOT NULL AUTO_INCREMENT, `firstName` varchar(255) NOT NULL, `lastName` varchar(255) NOT NULL, `middleName` varchar(255) NULL DEFAULT 'Unknown', `isActive` tinyint NOT NULL DEFAULT 1, PRIMARY KEY (`id`)) ENGINE=InnoDB");
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      "CREATE TABLE `user` (`id` int NOT NULL AUTO_INCREMENT, `firstName` varchar(255) NOT NULL, `lastName` varchar(255) NOT NULL, `middleName` varchar(255) NULL DEFAULT 'Unknown', `isActive` tinyint NOT NULL DEFAULT 1, PRIMARY KEY (`id`)) ENGINE=InnoDB",
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("DROP TABLE `user`");
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query("DROP TABLE `user`");
+  }
 }
