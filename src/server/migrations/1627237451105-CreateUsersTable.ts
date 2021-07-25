92231af62192fdb84f8025e153a4a995d14b76e1
Max Kayander <maxim.kayander1@gmail.com>
2021-07-25 21:28:19 +0300

    With ts-node we can use typescript entity and migration files.

--- src/server/migrations/1627237451105-CreateUsersTable.ts
+++ src/server/migrations/1627237451105-CreateUsersTable.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateUsersTable1627236821753 implements MigrationInterface {
-    name = 'CreateUsersTable1627236821753'
+export class CreateUsersTable1627237451105 implements MigrationInterface {
+    name = 'CreateUsersTable1627237451105'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE `user` (`id` int NOT NULL AUTO_INCREMENT, `firstName` varchar(255) NOT NULL, `lastName` varchar(255) NOT NULL, `middleName` varchar(255) NULL DEFAULT 'Unknown', `isActive` tinyint NOT NULL DEFAULT 1, PRIMARY KEY (`id`)) ENGINE=InnoDB");
